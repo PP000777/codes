56e5636a98c5eb5fdc636c0c7e86bcb96f5f3090
PP000777 <bia.silva007@hotmail.com>
2025-04-02 09:53:51 -0300

    Criando rotas do servidor

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 const http = require('http')
-const { soma } = require('./teste')
+const url = require('url') 
+const { soma, subtracao, multiplicacao, divisao } = require('./teste')
 
 const PORT = 8000
 
-
 const server = http.createServer((req, res) => {
     const urlCapturada = url.parse(req.url, true)
-    const {query} = urlCapturada
-    let resultado = soma(Number(query.a), Number(query.b))
-    res.end(`O valor final é ${resultado}`) 
-})
+    console.log(urlCapturada)
 
+    const { query, pathname } = urlCapturada
+    let resultado = 0
+
+    if (pathname === '/soma') {
+        resultado = soma(Number(query.a), Number(query.b))
+    } else if (pathname === '/subtracao') {
+        resultado = subtracao(Number(query.a), Number(query.b))
+    } else if (pathname === '/divisao') { 
+        resultado = divisao(Number(query.a), Number(query.b))
+    } else if (pathname === '/multiplicacao') { 
+        resultado = multiplicacao(Number(query.a), Number(query.b))
+    } else {
+        res.statusCode = 404
+        res.end("Página não encontrada")
+        return
+    }
+
+    res.end(`O valor final é ${resultado}`)
+})
 
 server.listen(PORT, () => {
     console.log(`O servidor foi inicializado na porta ${PORT}`)
--- teste.js
+++ teste.js
@@ -1,5 +1,14 @@
 function soma(a,b){
     return a + b
 }
+function subtracao(a,b){
+    return a-b
+}
+function multiplicacao(a,b){
+    return a*b
+}
+function divisao(a,b){
+    return a/b
+}
 
-module.exports = { soma }
+module.exports = { soma, subtracao,multiplicacao,divisao }
